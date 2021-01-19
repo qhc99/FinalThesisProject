754d37e994cb18a77c039081ccb1042a72c993db
nathan <1136026744@qq.com>
2021-01-19 09:57:35 +0800

    temp test pass

--- yolov5-master/predict.py
+++ yolov5-master/predict.py
@@ -4,8 +4,9 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
+import numpy as np
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
+from utils.datasets import LoadStreams, LoadImages, letterbox
 from utils.general import check_img_size, non_max_suppression, scale_coords, set_logging
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device
@@ -17,12 +18,9 @@ def pipeline(source, weights, img_size=640,
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://'))
 
-    # Initialize
     set_logging()
     device = select_device('')
-
-    # Load model
-    model = attempt_load(weights, map_location=device).half()
+    model = load_model(weights, device)
     imgsz = check_img_size(img_size, s=model.stride.max())
 
     if webcam:
@@ -31,14 +29,36 @@ def pipeline(source, weights, img_size=640,
     else:
         dataset = LoadImages(source, img_size=imgsz)
 
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    names = get_names(model)
+    colors = get_colors(names)
 
     for path, img, im0s, vid_cap in dataset:
         img = img_transform(img, device)
         pred = model(img, augment=augment_arg)[0]
         pred = non_max_suppression(pred, conf_thres_arg, iou_thres_arg, classes=classes_arg, agnostic=agnostic_nms_arg)
-        paint_result(pred, img, im0s, path, dataset, names, colors, img_window, webcam)
+        paint_result(pred, img, im0s, names, colors, path, img_window, webcam)
+
+
+def get_names(model):
+    return model.module.names if hasattr(model, 'module') else model.names
+
+
+def get_colors(names):
+    return [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+
+def load_model(weights_path, device):
+    return attempt_load(weights_path, map_location=device).half()
+
+
+def img_resize(img_origin, img_size=640):
+    # Padded resize
+    img = letterbox(img_origin, new_shape=img_size)[0]
+
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+    return img
 
 
 def img_transform(img, device):
@@ -50,13 +70,13 @@ def img_transform(img, device):
     return img
 
 
-def paint_result(pred, tensor_img, origin_img, path_img, dataset, names, colors, img_window=False, webcam=False):
+def paint_result(pred, tensor_img, origin_img, names, colors, path_img='', img_window=False, webcam=False):
     # Process detections
     for i, det in enumerate(pred):  # detections per image
         if webcam:  # batch_size >= 1
-            p, s, im0, frame = path_img[i], '%g: ' % i, origin_img[i].copy(), dataset.count
+            p, s, im0 = path_img[i], '%g: ' % i, origin_img[i].copy()
         else:
-            p, s, im0, frame = path_img, '', origin_img, getattr(dataset, 'frame', 0)
+            p, s, im0 = path_img, '', origin_img
         p = Path(p)  # to Path
         s += '%gx%g ' % tensor_img.shape[2:]  # print string
         if len(det):
@@ -66,7 +86,7 @@ def paint_result(pred, tensor_img, origin_img, path_img, dataset, names, colors,
             for *xyxy, conf, cls in reversed(det):
                 label = f'{names[int(cls)]} {conf:.2f}'
                 plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-            # Stream results
+
             if img_window:
                 if not webcam:
                     cv2.imshow(str(p), im0)
--- yolov5-master/temp.py
+++ yolov5-master/temp.py
@@ -1,22 +1,24 @@
 import cv2
+import torch.backends.cudnn as cudnn
+from utils.torch_utils import select_device
+from predict import load_model, get_names, get_colors, img_resize, img_transform, paint_result
+from utils.general import non_max_suppression
 
 
-def temp1():
+def temp1(weights_path, conf_thres=0.25, iou_thres=0.45,):
     cap = cv2.VideoCapture(0)
+    device = select_device('')
+    model = load_model(weights_path, device)
+    cudnn.benchmark = True
+    names = get_names(model)
+    colors = get_colors(names)
 
     while cap.isOpened():
         _, img = cap.read()
-        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-        # for (x, y, w, h) in faces:
-        #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-        #     roi_gray = gray[y:y + h, x:x + w]
-        #     roi_color = img[y:y + h, x:x + w]
-        #     eyes = eye_cascade.detectMultiScale(roi_gray)
-        #     for (ex, ey, ew, eh) in eyes:
-        #         cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
-
+        tensor_img = img_transform(img_resize(img, 640), device)
+        pred = model(tensor_img)[0]
+        pred = non_max_suppression(pred, conf_thres, iou_thres)
+        paint_result(pred, tensor_img, img, names, colors)
         cv2.imshow('img', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -25,4 +27,4 @@ def temp1():
 
 
 if __name__ == "__main__":
-    temp1()
+    temp1('parameters/original/yolov5s.pt',)
