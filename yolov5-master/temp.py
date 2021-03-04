7046552d000eaad5d0345c3f0a6fab5e5474ed0b
nathan <1136026744@qq.com>
2021-03-04 10:45:41 +0800

    save

--- yolov5-master/temp.py
+++ yolov5-master/temp.py
@@ -3,9 +3,11 @@ import torch.backends.cudnn as cudnn
 from utils.torch_utils import select_device
 from predict import load_model, get_names, get_colors, img_resize, img_transform, paint_result
 from utils.general import non_max_suppression
+import os
 
 
-def temp1(weights_path, conf_thres=0.25, iou_thres=0.45,):
+# FPS test
+def FPS_Test(weights_path, conf_thres=0.25, iou_thres=0.45,):
     cap = cv2.VideoCapture(0)
     device = select_device('')
     model = load_model(weights_path, device)
@@ -26,5 +28,19 @@ def temp1(weights_path, conf_thres=0.25, iou_thres=0.45,):
     cap.release()
 
 
+def ModelCheck():
+    imgs_folder_path = os.getcwd() + "\\dataset\\TrafficBlockSign\\pos\\img"
+    img_names_list = os.listdir(imgs_folder_path)
+    for img_name in img_names_list:
+        img_path = imgs_folder_path + "\\" + img_name
+        img = cv2.imread(img_path, 0)
+        try:
+            cv2.imshow("temp", img)
+            cv2.waitKey(3000)
+        except cv2.error:
+            print(img_path)
+
+
 if __name__ == "__main__":
-    temp1('parameters/original/yolov5s.pt',)
+    # FPS_Test('parameters/original/yolov5s.pt',)
+    ModelCheck()
