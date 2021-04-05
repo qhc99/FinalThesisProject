96638afda6212a5c0183bfcdc62d0c9d4c587a98
nathan <1136026744@qq.com>
2021-04-05 20:36:16 +0800

    assemble two models

--- yolov5-master/scripts.py
+++ yolov5-master/scripts.py
@@ -13,7 +13,7 @@ NEG_IMGS_FOLDER_PATH = "../../dataset/TrafficBlockSign/neg_imgs/imgs"
 POS_IMGS_FOLDER_PATH = "../../dataset/TrafficBlockSign/pos_imgs/img"
 POS_HARDIMGS_FOLDER_PATH = "../../dataset/TrafficBlockSign/pos_imgs/hard_imgs"
 
-MODEL_NUM = 2
+MODEL_NUM = 3
 
 # 1: 639
 # 3: 381
@@ -26,6 +26,7 @@ YOLOV5S_PATH = "./parameters/original/yolov5s.pt"
 
 def paint_interested_result(pred, tensor_img, origin_img, names, colors, path_img='', img_window=False, webcam=False):
     # Process detections
+    painted = False
     for i, det in enumerate(pred):  # detections per image
         if webcam:  # batch_size >= 1
             p, s, im0 = path_img[i], '%g: ' % i, origin_img[i].copy()
@@ -40,6 +41,7 @@ def paint_interested_result(pred, tensor_img, origin_img, names, colors, path_im
             # reversed(det)
             for (*xyxy, conf, cls) in reversed(det):
                 if int(cls.item()) in {0, 1, 2, 3, 5, 7}:
+                    painted = True
                     label = f'{names[int(cls)]} {conf:.2f}'
                     plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
@@ -51,7 +53,7 @@ def paint_interested_result(pred, tensor_img, origin_img, names, colors, path_im
                         cv2.destroyAllWindows()
                 else:
                     raise Exception("not implement.")
-    return origin_img
+    return painted
 
 
 def RunYoloModel(conf_thres=0.25, iou_thres=0.45, compute_exe_time=False):
@@ -75,7 +77,7 @@ def RunYoloModel(conf_thres=0.25, iou_thres=0.45, compute_exe_time=False):
             t2 = time.time()
             # noinspection PyUnboundLocalVariable
             print((t2 - t1) * 1000, "ms")
-        img = paint_interested_result(pred, tensor_img, img, MODEL_OUTPUT_NAMES, MODEL_OUTPUT_COLOR)
+        paint_interested_result(pred, tensor_img, img, MODEL_OUTPUT_NAMES, MODEL_OUTPUT_COLOR)
         cv2.imshow('img', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -85,23 +87,23 @@ def RunYoloModel(conf_thres=0.25, iou_thres=0.45, compute_exe_time=False):
 
 def RunSignModel(IMGS_DIR_PATH=POS_IMGS_FOLDER_PATH, show=False, compute_exe_time=False):
     SIGN_CLASSIFIER = cv2.CascadeClassifier(CASCADE_FILE_PATH)
-    pos_imgs_folder_path = os.path.join(os.getcwd(), IMGS_DIR_PATH)
-    img_names_list = os.listdir(pos_imgs_folder_path)
+    imgs_folder_path = os.path.join(os.getcwd(), IMGS_DIR_PATH)
+    img_names_list = os.listdir(imgs_folder_path)
     count = 0
     for img_name in img_names_list:
-        img_path = pos_imgs_folder_path + "\\" + img_name
+        img_path = imgs_folder_path + "\\" + img_name
         img = cv2.imread(img_path)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         if compute_exe_time:
             t1 = time.time()
-        res = SIGN_CLASSIFIER.detectMultiScale(gray, 1.1, 1)
+        detect = SIGN_CLASSIFIER.detectMultiScale(gray, 1.1, 1)
         if compute_exe_time:
             t2 = time.time()
             # noinspection PyUnboundLocalVariable
             print((t2 - t1) * 1000, "ms")
 
-        if len(res) > 0:
-            for (x, y, w, h) in res:
+        if len(detect) > 0:
+            for (x, y, w, h) in detect:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             print(img_name)
             count += 1
@@ -114,8 +116,83 @@ def RunSignModel(IMGS_DIR_PATH=POS_IMGS_FOLDER_PATH, show=False, compute_exe_tim
     print(count)
 
 
+def RunModel(conf_thres=0.25, iou_thres=0.45, CAMERA=True, IMG_FOLDER_PATH=None):
+    if not CAMERA and (IMG_FOLDER_PATH is None):
+        raise Exception
+    SIGN_CLASSIFIER = cv2.CascadeClassifier(CASCADE_FILE_PATH)
+    GPU_DEVICE = select_device('')
+    YOLO_MODEL = load_model(YOLOV5S_PATH, GPU_DEVICE)
+    # 0 1 2 3 5 7
+    MODEL_OUTPUT_NAMES = get_names(YOLO_MODEL)
+    MODEL_OUTPUT_COLOR = get_colors(MODEL_OUTPUT_NAMES)
+    cudnn.benchmark = True
+
+    if CAMERA:
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened():
+            _, img = cap.read()
+
+            runTwoModelsAndPaint(img,
+                                 YOLO_MODEL, GPU_DEVICE,
+                                 conf_thres, iou_thres,
+                                 MODEL_OUTPUT_NAMES, MODEL_OUTPUT_COLOR,
+                                 SIGN_CLASSIFIER)
+
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    else:
+        imgs_folder_path = os.path.join(os.getcwd(), IMG_FOLDER_PATH)
+        img_names_list = os.listdir(imgs_folder_path)
+        for img_name in img_names_list:
+            img_path = os.path.join(imgs_folder_path, img_name)
+            img = cv2.imread(img_path)
+
+            yolo_painted, sign_painted = runTwoModelsAndPaint(img,
+                                                              YOLO_MODEL, GPU_DEVICE,
+                                                              conf_thres, iou_thres,
+                                                              MODEL_OUTPUT_NAMES, MODEL_OUTPUT_COLOR,
+                                                              SIGN_CLASSIFIER)
+
+            try:
+                if yolo_painted or sign_painted:
+                    cv2.imshow("detect", img)
+                    cv2.waitKey(3000)
+            except cv2.error:
+                print(img_path)
+
+
+def runTwoModelsAndPaint(img,
+                         YOLO_MODEL, GPU_DEVICE,
+                         conf_thres, iou_thres,
+                         MODEL_OUTPUT_NAMES, MODEL_OUTPUT_COLOR,
+                         SIGN_CLASSIFIER):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # run yolo
+    tensor_img = img_transform(img_resize(img, 640), GPU_DEVICE)
+    pred = YOLO_MODEL(tensor_img)[0]
+    pred = non_max_suppression(pred, conf_thres, iou_thres)
+
+    # run cascade
+    detect = SIGN_CLASSIFIER.detectMultiScale(gray, 1.1, 1)
+
+    sign_painted = False
+    # paint cascade
+    if len(detect) > 0:
+        sign_painted = True
+        for (x, y, w, h) in detect:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # paint yolo
+    yolo_painted = paint_interested_result(pred, tensor_img, img, MODEL_OUTPUT_NAMES, MODEL_OUTPUT_COLOR)
+
+    return yolo_painted, sign_painted
+
+
 if __name__ == "__main__":
-    RunYoloModel()
-    # RunSignModel(POS_IMGS_FOLDER_PATH, show=False, compute_exe_time=True)
+    # RunYoloModel(compute_exe_time=True)
+    # RunSignModel(NEG_IMGS_FOLDER_PATH, show=True, compute_exe_time=True)
+    RunModel()
 
     print("end")
